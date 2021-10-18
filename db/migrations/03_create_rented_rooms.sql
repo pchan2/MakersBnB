d829b875335c16aaa7d35c4b9311e3f19132d40a
Patrick Chan <q_p18@hotmail.com>
2021-10-18 18:16:58 +0100

    Move 3 psql docs to branch "patrick/psql"

--- db/migrations/03_create_rented_rooms.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE rented_rooms (
-  id                SERIAL PRIMARY KEY,
-  room_id           INT REFERENCES rooms(id),
-  user_id           INT REFERENCES users(id),
-  occupy_date       DATE,
-  vacate_date       DATE,
-  request_approved  BOOLEAN DEFAULT FALSE
-);
