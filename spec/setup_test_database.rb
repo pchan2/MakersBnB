7e01ed037f6a21686a21a94fcd649304470328d0
Sunny Donaldson <sunnydonaldson1@gmail.com>
2021-10-19 12:25:28 +0100

    setup_test_database written

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -7,6 +7,40 @@ def setup_test_database
 
   # Clear the bookmarks table
   connection.exec("TRUNCATE rented_rooms, rooms, users")
+  connection.exec("ALTER SEQUENCE users_id_seq RESTART WITH 1;")
+  connection.exec("ALTER SEQUENCE rooms_id_seq RESTART WITH 1;")
+  
+  populate_users(connection)
+  populate_rooms(connection)
+  populate_rented_rooms(connection)
+end
 
+def populate_users(connection)
+  #populating users
+  connection.exec("INSERT INTO users(name) VALUES('Dave');")
+  connection.exec("INSERT INTO users(name) VALUES('Susan');")
+end
+
+def populate_rooms(connection)
+
+
+  connection.exec("INSERT INTO rooms(user_id, title, description, price, location)
+    VALUES(2, 'castle', 'a victorian folley', 80, 'Scotland' );"
+  )
+
+  connection.exec("INSERT INTO rooms(user_id, title, description, price, location)
+  VALUES(1, 'windmill', 'A really old windmill', 59.98, 'Essex');"
+)
+end
+
+def populate_rented_rooms(connection)
+  connection.exec("INSERT INTO rented_rooms(room_id, user_id, occupied_date)
+    VALUES(1, 1, '2001-10-19');"
+  )
+
+  connection.exec("INSERT INTO rented_rooms(room_id, user_id, occupied_date)
+    VALUES(2, 2, '2021-12-25');"
+  )
 
 end
+
