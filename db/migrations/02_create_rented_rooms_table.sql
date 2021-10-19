1adb534ea20118a95cf190fef7a2ebfe521ee6df
Sunny <54858282+sunnydonaldson@users.noreply.github.com>
2021-10-19 09:38:59 +0100

    Create 02_create_rented_rooms_table.sql

---
+++ db/migrations/02_create_rented_rooms_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE rooms(
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  title VARCHAR(30),
+  description VARCHAR(200),
+  price MONEY NOT NULL,
+  location, VARCHAR(30)
+);
