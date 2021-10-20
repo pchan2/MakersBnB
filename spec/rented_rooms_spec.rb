363e3d464ac7087ed5a73b08721829df01fd1f9d
Patrick Chan <q_p18@hotmail.com>
2021-10-20 16:15:55 +0100

    Remove "p"

--- spec/rented_rooms_spec.rb
+++ spec/rented_rooms_spec.rb
@@ -3,7 +3,7 @@ require_relative "../lib/rented_rooms"
 describe Rented_rooms do
   describe "#request_room" do
     it "selects a room and creates a message for the owner" do
-      p rented_room = Rented_rooms.request_room(user_id: 1, room_id: 2, occupied_date: "2021-10-14")
+      rented_room = Rented_rooms.request_room(user_id: 1, room_id: 2, occupied_date: "2021-10-14")
 
       expect(rented_room.room_id).to eq "2"
       expect(rented_room.user_id).to eq "1"
