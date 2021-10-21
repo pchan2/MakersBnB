9ad68c671745fb4415920d06e0a0266a1609e869
Leo Brown <leo_brown_1@hotmail.com>
2021-10-21 12:14:27 +0100

    add method for signin

--- lib/user.rb
+++ lib/user.rb
@@ -1,19 +1,27 @@
-require 'pg'
+require "pg"
+require_relative "../spec/database_helpers"
 
 class User
-  attr_reader :id,:name
-  def initialize(id: , name:)
+  attr_reader :id, :name
+
+  def initialize(id:, name:)
     @id = id
     @name = name
   end
 
   def self.add(name:)
-    if ENV['RACK_ENV'] == 'test'
-      connection = PG.connect(dbname: 'makersbnb_test')
-    else
-      connection = PG.connect(dbname: 'makersbnb')
-    end
+    connection = database_switcher
     result = connection.exec_params("INSERT INTO users (name) VALUES($1) RETURNING id, name;", [name])
-    User.new(id: result[0]['id'], name: result[0]['name'])
+    User.new(id: result[0]["id"], name: result[0]["name"])
+  end
+
+  def self.signin(name:)
+    connection = database_switcher
+    result = connection.query("SELECT id FROM users WHERE name = '#{name}';")
+    if result.ntuples == 0
+      nil
+    else
+      result[0]["id"]
+    end
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,12 +1,20 @@
 require "user"
 
-describe '.add' do
-  it 'adds users to database' do
-    user = User.add(name: "Bob")
-    persisted_data = PG.connect(dbname: 'makersbnb_test').query("SELECT * FROM users WHERE id = #{user.id};")
+describe User do
+  describe "#.add" do
+    it "adds users to database" do
+      user = User.add(name: "Bob")
+      persisted_data = PG.connect(dbname: "makersbnb_test").query("SELECT * FROM users WHERE id = #{user.id};")
 
-    expect(user).to be_a User
-    expect(user.id).to eq persisted_data.first['id']
-    expect(user.name).to eq "Bob"
+      expect(user).to be_a User
+      expect(user.id).to eq persisted_data.first["id"]
+      expect(user.name).to eq "Bob"
+    end
+  end
+
+  describe "#.signin" do
+    it "recognise an existing user" do
+      expect(User.signin(name: "Dave")).to eq "1"
+    end
   end
 end
